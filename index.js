f9070e8517a433b01323d44be145f9a50c148361
krishna pal <kpgangwar8909087801@gmail.com>
2024-07-03 16:14:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+
+import express from "express";
+import tweetRoutes from "./src/features/tweet/tweet.routes.js";
+const app = express();
+
+// TODO: Refactor these route handlers into tweet.routes.js file using express Router --------->>>>
+
+
+// app.post("/api/tweets", createTweet);
+// Write your code here
+app.use("/api/tweets", tweetRoutes);
+
+
+// -------------->>>>>>>>>>>>>>>>>>>
+
+// middleware for accessing these routes after refactoring
+// app.use("/api/tweets", tweetRoutes);
+
+app.listen(5000, () => {
+  console.log("server is listening at port 5000");
+});
